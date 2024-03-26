7f0d87d120450332d63873243b68f499b603248d
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-03-26 23:39:06 +0000

    fix: gitsigns not starting

--- .config/nvim/lua/core/plugins/git.lua
+++ .config/nvim/lua/core/plugins/git.lua
@@ -1,12 +1,12 @@
 return {
     "tpope/vim-fugitive",
     {
-        'lewis6991/gitsigns.nvim',
-        setup = {
-            current_line_blame = true
-        },
-        keys = {
-            {"n", "<leader>gh", ":Gitsigns preview_hunk<CR>"}
-        }
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup{
+                current_line_blame = true
+            }
+            vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
+        end
     },
 }
