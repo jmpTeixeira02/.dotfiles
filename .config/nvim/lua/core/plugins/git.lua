ab1f668c2d91110fde1e1a1d6642b64ae79446e9
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-03-29 14:15:30 +0000

    feat: added lazygit to nvim

--- .config/nvim/lua/core/plugins/git.lua
+++ .config/nvim/lua/core/plugins/git.lua
@@ -3,10 +3,29 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup{
+            require('gitsigns').setup {
                 current_line_blame = true
             }
             vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
         end
     },
+    {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            cmd = {
+                    "LazyGit",
+                    "LazyGitConfig",
+                    "LazyGitCurrentFile",
+                    "LazyGitFilter",
+                    "LazyGitFilterCurrentFile",
+                },
+                require("lazy").setup({
+
+                    vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
+                })
+        end
+    }
 }
