c347621281bbcc03ab02eb1149fd5eacb95e8e3d
João Teixeira <jteixeira@pod.sys.sibs.pt>
2024-10-18 11:59:25 +0100

    feat: oil natural order and hide .. and .git

--- .config/nvim/lua/core/plugins/oil.lua
+++ .config/nvim/lua/core/plugins/oil.lua
@@ -5,14 +5,18 @@ return {
     config = function()
         require("oil").setup({
             view_options = {
-                show_hidden = true
+                show_hidden = true,
+                natural_order = true,
+                is_always_hidden = function(name, _)
+                    return name == ".." or name == ".git"
+                end,
             },
             keymaps = {
                 ["<C-s>"] = "false",
                 ["<C-t>"] = "false",
                 ["<C-h>"] = "false",
                 ["<C-c>"] = "false",
-            }
+            },
         })
     end,
 }
