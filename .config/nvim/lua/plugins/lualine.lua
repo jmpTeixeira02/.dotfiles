903a6e01d678fe0f2cd61f55363a0701b348ccfc
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-12-01 00:38:18 +0000

    feat: lualine buffers

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,8 @@ return {
     options = {
       component_separators = "",
       always_divide_middle = true,
-      globalstatus = false,
+      globalstatus = vim.o.laststatus == 3,
+      disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
     },
     sections = {
       lualine_a = {
@@ -26,16 +27,44 @@ return {
             alternate_file = "",
             directory = "",
           },
-          mode = 0,
+          mode = 2,
         },
       },
       lualine_c = {
-        { "filename", path = 1 },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path() },
       },
-      lualine_x = { "filetype" },
+      lualine_x = {
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = function() return LazyVim.ui.fg("Debug") end,
+          },
+          -- stylua: ignore
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function() return LazyVim.ui.fg("Special") end,
+          },
+      },
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
-    extensions = { "nvim-dap-ui", "oil", "trouble" },
+    extensions = { "nvim-dap-ui", "trouble", "mason" },
   },
+  keys = function()
+    local keys = {}
+
+    for i = 1, 8 do
+      table.insert(keys, {
+        "<leader>b" .. i,
+        function()
+          vim.cmd("LualineBuffersJump! " .. i)
+        end,
+        desc = "Buffer to " .. i,
+      })
+    end
+    return keys
+  end,
 }
