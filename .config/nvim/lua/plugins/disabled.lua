2205537ced1555474f5d76300621a858248c393f
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-12-01 11:49:05 +0000

    chore: use default snacks and add back tokyonight color

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "stevearc/conform.nvim", enabled = false },
   { "mfussenegger/nvim-lint", enabled = false },
