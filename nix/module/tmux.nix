be540bd78b8b28029c71aeaf36890d875e88c0de
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-12-09 22:28:27 +0000

    feat: nix tmux

--- nix/module/tmux.nix
+++ nix/module/tmux.nix
@@ -1,28 +1,29 @@
 { config, pkgs, ... }:
 
-# let
-#   tmux-kanagawa = {
-#     pluginName = "tmux-kanagawa";
-#     src = pkgs.fetchFromGitHub {
-#       owner = "Nybkox";
-#       repo = "tmux-kanagawa";
-#       rev = "master";
-#       sha256 = "0d2db8d95e1b74643a06802043c7000a79ba0a0a";
-#     };
-#   };
-# in
+let
+  tmux-kanagawa = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-kanagawa";
+    version = "stable-2024-09-12";
+    src = pkgs.fetchFromGitHub {
+      owner = "Nybkox";
+      repo = "tmux-kanagawa";
+      rev = "master";
+      sha256 = "sha256-9S4HQHuECGLPLdPishmwEO0twdeQ6mZQfIMNFFDkgQ8=";
+    };
+  };
+in
 {
   home.packages = with pkgs; [
     tmux
     tmuxPlugins.vim-tmux-navigator
     tmuxPlugins.sensible
     tmuxPlugins.continuum
-    # tmux-kanagawa
+    tmux-kanagawa
   ];
 
   home.file = {
-    ".config/tmux".source =
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/tmux";
+    ".config/tmux.conf".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/tmux/tmux.conf";
   };
 
   programs.home-manager.enable = true;
