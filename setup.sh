05e3b283666da11ec08da3abdaef1fef96eed5e1
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-12-09 22:35:32 +0000

    feat: setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-home-manager switch --flake ./nix/#joao
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+nix run home-manager -- switch --flake ./nix/#joao
