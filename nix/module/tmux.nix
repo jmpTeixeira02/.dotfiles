9b866f7cd40061c3e2c213f2198564a3fc7533b2
Joao Teixeira <jteixeira@pod.sys.sibs.pt>
2024-12-19 12:30:06 +0000

    fix: error on linux systems

--- nix/module/tmux.nix
+++ nix/module/tmux.nix
@@ -22,28 +22,25 @@ in
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
-    shell = "${pkgs.zsh}/bin/zsh";
     baseIndex = 1;
     keyMode = "vi";
     mouse = true;
-    clock24 = true;
-
     plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = kanagawa;
-        extraConfig = ''
-          set -g @kanagawa-plugins "ssh-session  time"
-          set -g @kanagawa-show-timezone false
-          set -g @kanagawa-theme dragon
-          set -g @kanagawa-day-month true
-          set -g @kanagawa-show-powerline true
-          set -g @kanagawa-military-time true
-          set -g @kanagawa-show-left-icon session
-          # set -g @kanagawa-time-colors "cyan gray"
-          set -g @kanagawa-time-format " %R"
-          set -g @kanagawa-ignore-window-colors true
+    {
+      plugin = kanagawa;
+      extraConfig = ''
+        set -g @kanagawa-plugins "ssh-session  time"
+        set -g @kanagawa-show-timezone false
+        set -g @kanagawa-theme dragon
+        set -g @kanagawa-day-month true
+        set -g @kanagawa-show-powerline true
+        set -g @kanagawa-military-time true
+        set -g @kanagawa-show-left-icon session
+        # set -g @kanagawa-time-colors "cyan gray"
+        set -g @kanagawa-time-format " %R"
+        set -g @kanagawa-ignore-window-colors true
         '';
-      }
+    }
       yank
       vim-tmux-navigator
       tmux-thumbs
@@ -52,7 +49,6 @@ in
       sensible
       continuum
     ];
-
     extraConfig = ''
       set-option -g status-position top
 
@@ -66,8 +62,6 @@ in
 
       set -g @resurrect-capture-pane-contents 'on' # allow tmux-ressurect to capture pane contents
       set -g @continuum-restore 'on' # enable tmux-continuum functionality
-
-      # run -b '~/.tmux/plugins/tpm/tpm'
     '';
   };
 
