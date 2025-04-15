c5aa57f9390409886bb89f16895b4fc671ad5fee
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2025-04-15 17:10:45 +0100

    feat: add snacks picker

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,41 +3,4 @@ return {
   opts = {
     inlay_hints = { enabled = false },
   },
-  keys = {
-    {
-      "<leader>fa",
-      function()
-        require("fzf-lua").files()
-      end,
-      desc = "Find files",
-    },
-    {
-      "<leader>ff",
-      function()
-        require("fzf-lua").git_files()
-      end,
-      desc = "Find git files",
-    },
-    {
-      "<leader>ca",
-      function()
-        require("fzf-lua").lsp_code_actions()
-      end,
-      desc = "Code Actions",
-    },
-    {
-      "<leader>ft",
-      function()
-        require("fzf-lua").treesitter()
-      end,
-      desc = "Tree sitter",
-    },
-    {
-      "<leader>fs",
-      function()
-        require("fzf-lua").live_grep()
-      end,
-      desc = "Find string",
-    },
-  },
 }
---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,36 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    { "<leader>fa", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    { "<leader>fA", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "Find Files (git-files)",
+    },
+    {
+      "<leader>uu",
+      function()
+        Snacks.picker.undo()
+      end,
+      desc = "Undotree",
+    },
+    {
+      "<leader>ca",
+      function()
+        require("fzf-lua").lsp_code_actions()
+      end,
+      desc = "Code Actions",
+    },
+    {
+      "<leader>ft",
+      function()
+        Snacks.picker.treesitter()
+      end,
+      desc = "Tree sitter",
+    },
+    { "<leader>fs", LazyVim.pick("grep"), desc = "Grep (Root Dir)" },
+  },
+}
