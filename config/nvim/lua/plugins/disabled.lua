80136763a7b2daf8139149f4f8324971e9b9712f
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2025-04-15 16:43:11 +0100

    fix: nil_ls warnings

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -3,6 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "stevearc/conform.nvim", enabled = true },
-  { "nvimtools/none-ls.nvim", enabled = false },
   { "mfussenegger/nvim-lint", enabled = false },
 }
