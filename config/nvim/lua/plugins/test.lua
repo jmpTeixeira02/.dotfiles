5ba8322c229285194b86fd092e9c5f093d74aea2
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2025-06-26 18:13:23 +0100

    feat: testify show tests on suite and coverage

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,7 +1,54 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
+  {
+    "andythigpen/nvim-coverage",
+    version = "*",
+    config = function()
+      require("coverage").setup({
+        auto_reload = true,
+        lang = {
+          ["go"] = {
+            coverage_file = "cover.out",
+          },
+        },
+      })
+    end,
+    keys = function()
+      local keys = {
+        {
+          "<leader>ct",
+          function()
+            require("coverage"):toggle()
+          end,
+          desc = "Toggle Coverage",
+        },
+        {
+          "<leader>cL",
+          function()
+            require("coverage"):load()
+          end,
+          desc = "Load Coverage",
+        },
+        {
+          "<leader>co",
+          function()
+            require("coverage"):summary()
+          end,
+          desc = "Show Coverage Output",
+        },
+      }
+
+      return keys
+    end,
+  },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary", "neotest-golang" } },
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          dap_go_enabled = true,
+          testify_enabled = true,
+        },
+      },
+    },
   },
 }
