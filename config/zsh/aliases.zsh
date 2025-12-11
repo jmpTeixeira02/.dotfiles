3088f51a477dde27b6d5aab2795d886fa593bdbf
Joao Teiexeira <joaompt2002@hotmail.com>
2025-12-11 20:51:45 +0000

    chore: move alias out of zshrc

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,7 @@
+# Alias
+alias vim="nvim"
+alias vi="nvim"
+alias src="source ~/.zshrc"
+alias tree="eza --icons --tree"
+alias cat='bat --paging=never'
+alias lg='lazygit'
--- nix/module/shell.nix
+++ nix/module/shell.nix
@@ -12,6 +12,8 @@
   home.file = {
     ".zshrc".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/zsh/zshrc";
+    ".config/zsh".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/zsh";
     ".config/starship".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/starship";
   };
