fe19d8c270869ac31079a97339518bb47ee352f7
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2025-12-12 16:20:54 +0000

    chore: separate update and reload

--- reload.sh
+++ reload.sh
@@ -8,5 +8,4 @@ export NIX_USER="$1"
 export FLAKE_USER=$(whoami)
 export FLAKE_HOME=$(echo $HOME)
 export NIXPKGS_ALLOW_BROKEN=1
-nix flake update --flake ./nix
 nix run home-manager -- switch --flake ./nix/#${NIX_USER} --impure
