14bd0b973667e78e339f91cc7fa60df0bac3423e
Joao Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2025-12-12 16:22:51 +0000

    chore: fix lg alias

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -4,4 +4,16 @@ alias vi="nvim"
 alias src="source ~/.zshrc"
 alias tree="eza --icons --tree"
 alias cat='bat --paging=never'
-alias lg='lazygit'
+
+# Lazygit switch dir on exit
+lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
+}
